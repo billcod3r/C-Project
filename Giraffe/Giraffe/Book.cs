92e36814474c2967eb0b0ff6f420362d2db8e279
Bill Calixte <billcalixte90@gmail.com>
2021-08-06 12:55:58 -0400

    First commit

---
+++ Giraffe/Giraffe/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Giraffe
+{
+    class Book
+    {
+        // class attributes
+        public string title;
+        public string author;
+        public int pages;
+
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+
+    }
+}
---
+++ Giraffe/Giraffe/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Math.Sqrt(144));
+
+            
+
+            Console.ReadLine();
+        }
+
+       
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+        }
+    }
+
